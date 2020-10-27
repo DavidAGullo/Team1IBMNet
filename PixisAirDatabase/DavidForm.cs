70b24b14822164d361546b37a69210de8a871f3a
David Gullo <60278018+DavidAGullo@users.noreply.github.com>
2020-10-26 19:31:07 -0500

    Start Project

--- PixisAirDatabase/DavidForm.cs
+++ PixisAirDatabase/DavidForm.cs
@@ -10,17 +10,11 @@ using System.Windows.Forms;
 
 namespace PixisAirDatabase
 {
-    //Test Comments!
-    public partial class Form1 : Form
+    public partial class DavidForm : Form
     {
-        public Form1()
+        public DavidForm()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            //Stuff
         }
     }
 }
---
+++ PixisAirDatabase/mainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixisAirDatabase
+{
+    //Main Form Created By David
+    public partial class mainForm : Form
+    {
+        DavidForm dForm = new DavidForm();
+        SeanForm sForm = new SeanForm();
+        AlexisForm aForm = new AlexisForm();
+        JoshuaForm jForm = new JoshuaForm();
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dForm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sForm.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            aForm.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            jForm.Show();
+        }
+    }
+}
